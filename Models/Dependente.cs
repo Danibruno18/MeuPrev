c22f963e4adbfcf25f15a729fcd24acfd58467fe
bruno daniel <danibrunoo18@gmail.com>
2024-12-02 20:33:09 -0300

    criadas as ultimas classes

---
+++ Models/Dependente.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuPrev.Models
+{
+    public class Dependente
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required]
+        [MaxLength(100)] 
+        public string Nome { get; set; } = string.Empty;
+
+        [ForeignKey("Pai")]
+        public int? PaiId { get; set; } 
+
+        [ForeignKey("Mae")]
+        public int? MaeId { get; set; }
+
+        public Pessoa? Pai { get; set; }
+        public Pessoa? Mae { get; set; }
+    }
+}
---
+++ Models/TempoAverbado.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuPrev.Models
+{
+    public class TempoAverbado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Funcionario")]
+        public int Matricula { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Cargo { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime DataAdmissao { get; set; }
+
+        [Required]
+        public DateTime DataDemissao { get; set; }
+
+        public int TotalDias { get; set; }
+
+        public Funcionario? Funcionario { get; set; }
+    }
+}
